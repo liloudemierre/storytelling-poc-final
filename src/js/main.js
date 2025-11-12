add339309ecd5e1d5f1af27aa4efd25415a29235
liloudemierre <161479296+liloudemierre@users.noreply.github.com>
2025-11-12 17:52:36 +0100

    animation sequence 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,15 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
-// const tlMain = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: "body",
-//     start: "top top",
-//     // end: "bottom+=150% top",
-//     pin: true,
-//     scrub: 5,
-//     markers: false,
-//   },
-// });
+
+// Séquence 1
 const tl = gsap.timeline({
   scrollTrigger: {
     trigger: ".oiseau",
     start: "top top",
-    end: "bottom+=150% top",
+    end: "bottom+=1500% top",
     pin: true,
-    scrub: 5,
+    scrub: true,
     markers: false,
   },
 });
@@ -26,98 +18,106 @@ tl.to(".oiseau-image", { x: "300%", y: "-200%", duration: 6, ease: "none" })
   .to(".crique-image", { y: "-85%", duration: 6, ease: "none" }, "+=5")
   .to(
     ".crique-text",
-    {
-      ease: "sine.inOut",
-      opacity: "100",
-      duration: 3,
-    },
+    { ease: "sine.inOut", opacity: "100", duration: 3 },
     "+=2"
   )
-  .to(".crique-image", {
-    scale: "20",
-    x: "720%",
-    duration: 7,
-    delay: "10",
-  })
+  .to(".crique-image", { scale: "20", x: "720%", duration: 7, delay: "10" })
   .to(".crique-text", { x: "1000%", duration: 7, delay: "-7" })
   .to(".oiseau", { opacity: "0" });
 
+// Séquence 2
 const tl2 = gsap.timeline({
   scrollTrigger: {
     trigger: ".vacancier",
     start: "top top",
-    end: "bottom+=150% top",
+    end: "bottom+=500% top",
     pin: true,
-    scrub: 5,
+    scrub: true,
     markers: false,
   },
 });
 
 tl2
-  .to(".vacancier-text", {
-    ease: "sine.inOut",
-    opacity: "100",
-  })
-  .to(".herbe-right", {
-    x: "-70%",
-    duration: "3",
-  })
-  .to(
-    ".herbe-left",
-    {
-      x: "70%",
-      duration: "3",
-    },
-    "-=3"
-  )
-  .to(
-    ".vacancier-right",
-    {
-      x: "-70%",
-      duration: "3",
-    },
-    "-=3"
-  )
+  .to(".vacancier-text", { ease: "sine.inOut", opacity: "100" })
+  .to(".herbe-right", { x: "-70%", duration: "3" })
+  .to(".herbe-left", { x: "70%", duration: "3" }, "-=3")
+  .to(".vacancier-right", { x: "-70%", duration: "3" }, "-=3")
+  .to(".vacancier-left", { x: "70%", duration: "3" }, "-=3")
+  .to(".vacancier-text", { duration: 2, ease: "sine.inOut", opacity: "0" })
+  .to(".herbe-right, .vacancier-right", { x: "70%", duration: "1" })
+  .to(".herbe-left, .vacancier-left", { x: "-70%" }, "-=1");
 
-  .to(
-    ".vacancier-left",
-    {
-      x: "70%",
-      duration: "3",
-    },
-    "-=3"
-  )
-  .to(".vacancier-text", {
-    duration: 2,
-    ease: "sine.inOut",
-    opacity: "0",
-  })
-  .to(".herbe-right, .vacancier-right", {
-    x: "70%",
-    duration: "1",
-  })
-  .to(
-    ".herbe-left, .vacancier-left",
-    {
-      x: "-70%",
-    },
-    "-=1"
-  );
-
+// Séquence 3
 const tl3 = gsap.timeline({
   scrollTrigger: {
     trigger: ".chateau",
     start: "top top",
-    end: "bottom+=150% top",
+    end: "bottom+=500% top",
     pin: true,
-    scrub: 5,
+    scrub: true,
     markers: false,
   },
 });
 
 tl3
   .to(".chateau-text", { ease: "sine.inOut", opacity: "100" })
-  .to(".chateau-image", { y: "-70%" });
-tlMain.add(tl);
-tlMain.add(tl2);
-tlMain.add(tl3);
+  .to(".chateau-image", { y: "-140%", duration: 10 })
+  .to(".chateau-phrase", { y: "-200%", duration: "5" }, "-=5")
+  .to(".chateau-image", { y: "-250%", duration: 10 })
+  .to(".chateau-image chateau-text", { opacity: "0" });
+
+// Vidéo
+const videoContainer = document.querySelector(".container.video");
+const video = document.querySelector("#Video");
+
+ScrollTrigger.create({
+  trigger: ".sequence-1",
+  start: "bottom bottom",
+  markers: true,
+  id: "Video",
+  onEnter: () => {
+    videoContainer.classList.add("video-visible");
+    video.play();
+  },
+  onLeave: () => {
+    videoContainer.classList.remove("video-visible");
+    video.pause();
+  },
+  onEnterBack: () => video.play(),
+  onLeaveBack: () => video.pause(),
+});
+
+video.addEventListener("ended", () => {
+  // Supprimer les anciens triggers
+  ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+
+  // Passer à la séquence suivante
+  document.querySelector(".sequence-1").classList.remove("visible");
+  document.querySelector(".sequence-2").classList.add("visible");
+
+  // Recalcul du scroll
+  ScrollTrigger.refresh();
+
+  // === Création du trigger pour la séquence 2 (issue) ===
+  const tl4 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".issue",
+      start: "top top",
+      end: "bottom+=200% top",
+      pin: true,
+      scrub: 5,
+      markers: true,
+    },
+  });
+
+  tl4
+    .to(".issue-soleil", { x: "100%", y: "-190%", duration: "2" })
+    .to(".issue-text", { x: "-30%", y: "430%", duration: "1" }, "-=2")
+    .to(".issue", {
+      scale: 20, // zoom de la "caméra"
+      x: "-380%", // ajuster pour centrer sur le soleil
+      y: "780%",
+      transformOrigin: "center center",
+      duration: 5,
+    });
+});
